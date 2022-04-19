70636c1761cdd2e9c1b2c412fbe4307da56bf682
AlexDoss42 <alex.doss42@gmail.com>
2022-04-18 23:35:05 -0600

    adds some simple react tests

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Todo() {
   return (
-    <div>Todo</div>
+    <div data-testid="todo-1">Todo</div>
   )
 }
 
--- src/components/__tests__/todo.test.js
+++ src/components/__tests__/todo.test.js
@@ -1,3 +1,10 @@
-test('test', () => {
-    expect(true).toBe(true);
+import { react, screen, cleaup, render } from '@testing-library/react';
+import Todo from '../Todo';
+
+
+test('should render todo component', () => {
+    render(<Todo />);
+    const todoElement = screen.getByTestId('todo-1');
+    expect(todoElement).toBeInTheDocument();
+    expect(todoElement).toHaveTextContent('Todo')
 })
