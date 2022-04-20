1519ecc0d4a777ffef05cf26b5af8ed55c68be6e
AlexDoss42 <alex.doss42@gmail.com>
2022-04-20 17:14:52 -0600

    adds some basic stuff for another tutorial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import './App.css';
+import Button from './components/Button';
+import Input from './components/Input';
 import Todo from './components/Todo';
 
 function App() {
+  const [showDiv, setShowDiv] = useState(true);
   const todos = [
     {
       id: 1,
@@ -21,6 +25,10 @@ function App() {
           return (<Todo todo={todo}/>)
         })
       }
+      <div>
+        <Input showDiv={showDiv} />
+        <Button />
+      </div>
     </div>
   );
 }
