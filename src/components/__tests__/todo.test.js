74d2c5b96b6f5fc94cad0420b4300f189cc59240
AlexDoss42 <alex.doss42@gmail.com>
2022-04-19 21:18:01 -0600

    fixes broken test number 2

--- src/components/__tests__/todo.test.js
+++ src/components/__tests__/todo.test.js
@@ -1,10 +1,33 @@
-import { react, screen, cleaup, render } from '@testing-library/react';
+import { screen, render, cleanup } from '@testing-library/react';
 import Todo from '../Todo';
+
+afterEach(() => {
+    cleanup();
+})
 
 
 test('should render todo component', () => {
-    render(<Todo />);
+    const todo = {
+        id: 1,
+        title: 'wash dishes',
+        completed: false
+      };
+    render(<Todo todo={todo}/>);
     const todoElement = screen.getByTestId('todo-1');
     expect(todoElement).toBeInTheDocument();
-    expect(todoElement).toHaveTextContent('Todo')
-})
+    expect(todoElement).toHaveTextContent('wash dishes');
+    expect(todoElement).not.toContainHTML('<strike>');
+});
+
+test('should render completed todo', () => {
+    const todo = {
+        id: 2,
+        title: 'wash car',
+        completed: true
+      };
+    render(<Todo todo={todo}/>);
+    const todoElement = screen.getByTestId('todo-2');
+    expect(todoElement).toBeInTheDocument();
+    expect(todoElement).toHaveTextContent('wash car');
+    expect(todoElement).toContainHTML('<div data-testid="todo-2"><strike><h1>wash car</h1></strike></div>');
+});
