57e8c9966b93636accc613b649543c33cc462ddf
AlexDoss42 <alex.doss42@gmail.com>
2022-04-20 18:06:43 -0600

    adds test to see if second button shows up after clicking the first

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Button from '../Button';
 
 describe("Button Component", () => {
@@ -8,5 +8,18 @@ describe("Button Component", () => {
         const button = screen.getByTestId("button");
         expect(button).toBeInTheDocument();
     });
-    
+
+    it("render 1 button before button click", () => {
+        render(<Button />)
+        const buttonList = screen.getAllByTestId("button");
+        expect(buttonList).toHaveLength(1);
+    });
+
+    it("render 2 button after button click", async () => {
+        render(<Button />)
+        const buttonList = screen.getAllByTestId("button");
+        await fireEvent.click(buttonList[0]);
+        expect(screen.getAllByTestId("button")).toHaveLength(2);
+    });
+
 });
